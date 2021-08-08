18060c06ad276ddd9554e3aa36dc82aa0b0b24e6
zdl1 <1925159557@qq.com>
2021-08-09 00:08:14 +0800

    这是我第一次用 IDEA 提交

---
+++ src/com/testGit01/test/TestGit01.java
@@ -0,0 +1,13 @@
+package com.testGit01.test;
+
+/**
+ * @author zdl TEL:13810218664 QQ:1925159557
+ * @project testGit01
+ * @description
+ * @create 2021-08-08 23:33
+ */
+public class TestGit01 {
+    public static void main(String[] args) {
+        System.out.println("This is my first git commit with IDEA!");
+    }
+}
